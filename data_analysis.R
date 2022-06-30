d5c5837eb1a570fcc64e9385d2322a74ed65173b
mdnnn <106843448+mdnnn@users.noreply.github.com>
2022-06-30 13:44:47 -0400

    Update data_analysis.R
    Multiple Regression

--- data_analysis.R
+++ data_analysis.R
@@ -1,1 +1,23 @@
 #data analysis
+
+fpath <- "/Users/dannima/Desktop/MBiotech/BTC1859/Group_Project/"
+df <- read.csv(paste0(fpath, "project_data.csv"))
+
+mod1 <- lm(Pittsburgh.Sleep.Quality.Index.Score ~ 
+             Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis + 
+             Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
+             Renal.Failure + Depression + Corticoid, data= df)
+summary(mod1)
+
+mod2 <- lm(Athens.Insomnia.Scale ~ 
+             Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis + 
+             Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
+             Renal.Failure + Depression + Corticoid, data= df)
+summary(mod2)
+ 
+
+mod3 <- lm(Epworth.Sleepiness.Scale ~ 
+             Gender + Age + BMI + Time.from.transplant + Liver.Diagnosis + 
+             Recurrence.of.disease + Rejection.graft.dysfunction + Any.fibrosis +
+             Renal.Failure + Depression + Corticoid, data= df)
+summary(mod3)
